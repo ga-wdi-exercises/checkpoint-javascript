b11c4f56791a77c6e70c47e98bb930d7725aa7d7
Dan McQuade <dtmcquade@gmail.com>
2017-10-23 09:30:06 -0400

    Complete checkpoint exercises

--- fundamentals.js
+++ fundamentals.js
@@ -3,29 +3,35 @@
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
 
-
+let foods = ['tacos', 'burgers', 'shrimp']
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
 
-
+let last = foods[foods.length - 1]
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
 
-
+let favoriteFoods = []
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
 
-
+for (let i = 0; i < foods.length; i++) {
+  favoriteFoods.push(foods[i])
+}
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
-
-
+let instructor = {
+  name: 'Zakk',
+  cohort: 'WDI19',
+  strengths: 'Web Development'
+}
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+instructor['has-office-hours'] = true
