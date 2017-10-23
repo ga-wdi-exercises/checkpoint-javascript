d5c29fe0dbc5d83bee620ceb53c947def160c56e
Wilner Charles <wilnercharles3@gmail.com>
2017-10-23 13:43:58 -0400

    updated errors

--- fundamentals.js
+++ fundamentals.js
@@ -3,42 +3,35 @@
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
 
-var foods = ['icecream','berries','chocolate',];
-
+let foods = ['icecream', 'candy', 'fish']
 
 // #2: Access the last item in the array and assign to a variable called `last`.
-// Type your solution immediately below this line:\
+// Type your solution immediately below this line:
 
-var last = foods[2];
-
-
-
-
+let last = foods[foods.length [1]]
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
-var favoriteFoods = [];
 
-
+let favoriteFoods = []
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
-for (var i = 0; i < foods.length; i++) {
-  foods[i]
+
+for (let i = 0; i < foods.length; i++) {
+  favoriteFoods.push(foods[i])
 }
-
-
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
-let isnstrutor = {
-  class: "wdi19"
-  members: "21"
-
-
+let instructor = {
+  name: 'Charles',
+  class: 'WDI19',
+  strengths: 'Web Development'
+}
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
-var newVal = "cc";
+instructor['has-office-hours'] = true
