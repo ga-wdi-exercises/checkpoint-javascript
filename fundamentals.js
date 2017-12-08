8d67f0a162f45119c5505220c8848ac2c0a475ec
Temicka Brown <mekapleio@yahoo.com>
2017-12-08 15:52:45 -0500

    ok1234

--- fundamentals.js
+++ fundamentals.js
@@ -9,20 +9,28 @@ var foods = ['pizza', 'biryani', 'chicken']
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
+ foods[2]
+
  var = foods[2]
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
 
-
+var favoriteFoods[]
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
-
+ for(i=0; i<3; i++)
 
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
+
+var inststuctor = {
+    name: 'Mike',
+    age: 26,
+    occupation: 'Instructor'
+}
 
 
 
