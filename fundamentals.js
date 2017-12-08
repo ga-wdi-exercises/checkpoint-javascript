b9215aa20891b4fc577030b41ff31a417c7f8a57
Kathy Lambert <Kathy.Lambert@LotusDesignsAgency.com>
2017-12-08 16:14:45 -0500

    1st attempt at hof.js

--- fundamentals.js
+++ fundamentals.js
@@ -2,26 +2,35 @@
 
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
-
+var foods = ['lamb', 'steak', 'potatoes']
+console.log(foods)
 
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-
+var last = foods[foods.length -1]
+console.log(last)
 
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
-
+var favoriteFoods = []
 
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
-
+for 
 
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
+var instructor = {
+  wdi20: ['mike', 'ali', 'james', 'max', 'zach'],
+  primaryLanguage: ['JavaScript', 'Python', 'PHP'],
+  priorTeachingExperience: ['yes', 'no'],
+  
+  consolelog(instructor)
+}
 
 
 
@@ -29,3 +38,6 @@
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+instructor.push('has-office-hours')
+console.log(foods)
+
