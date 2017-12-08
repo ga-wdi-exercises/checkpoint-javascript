87bce673c2165889d0711946b9c3fc504ee6ca40
Natalee C <COPENATALEE@ME.COM>
2017-12-08 16:28:45 -0500

    checkpoint

--- fundamentals.js
+++ fundamentals.js
@@ -2,30 +2,40 @@
 
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
+var foods = ['mango' + 'orange' + 'peach']
 
 
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
+console.log(foods[2])
+foods.push('last')
 
 
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
-
+var favoriteFoods = []
 
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
-
+for (let i = 0; i < 2; i++) {
+    console.log(i)
+  }
 
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
 
-
+var instructor = {
+    firstName: 'Jane',
+    lastName: 'Doe',
+    subject: 'English'
+};
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+instructor.age = "50";
