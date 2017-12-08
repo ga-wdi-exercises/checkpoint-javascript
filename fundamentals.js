dbe0122cafdd522ef6c1e00df9d54fa8b901eec7
TERRENCE CAMPBELL <TLCAMPBELL89@GMAIL.COM>
2017-12-08 16:25:13 -0500

    done

--- fundamentals.js
+++ fundamentals.js
@@ -2,30 +2,37 @@
 
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
-
+var foods = ['chicken', 'steak', 'pork']
+    console.log(foods.length)
 
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-
+var last = foods[2]
 
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
-
+var favoriteFoods = {}
 
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
-
-
+arr.push('foods') 
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
-
+var instructor = {
+    teachingStyle: 'dynamic',
+    onTime: true,
+    effective: true
+}
 
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+var instructor = {
+    has-office-hours: true 
+}
--- oojs.js
+++ oojs.js
@@ -5,8 +5,18 @@
 // - a `songs` property that is an empty array not determined by input (not passed into the constructor)
 // - an `addSong` method that adds a song (string) to the `songs` array
 // Type your solution immediately below this line:
+class Playlist {
+  constructor (title) {
+    this.songs = {}
+  }
+
+  addSong (newSong){
+    this.songs = newSong
+  }
+}
 
 
+const iPod = 
 
 
 
