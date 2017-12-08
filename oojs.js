755dd600d0555519ed75fa5ffde396132ab25029
Hammad Malik <hmd.node@gmail.com>
2017-12-08 15:47:42 -0500

    oojs.js second task completed
    that should do it. let me see if it passes all the tests now

--- oojs.js
+++ oojs.js
@@ -18,13 +18,11 @@ class Playlist {
 // #2: Create an instance of the Playlist class and set it to a variable called `myPlaylist`
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 // Type your solution immediately below this line:
-
-
-
-
+var myPlaylist = new Playlist('best playlist ever')
+myPlaylist.addSong('La Macarena')
 
 // NOTE: THE CODE BELOW IS FOR TESTING PURPOSES. DO NOT REMOVE OR ALTER.
-if(typeof Playlist !== 'undefined') {
+if (typeof Playlist !== 'undefined') {
   module.exports = {
     Playlist
   }
