74a5d32058f5a830817b2e981a7e4d4b331e5ef1
Francis Julian Addison <faddison@Franciss-MacBook.local>
2017-12-08 16:17:42 -0500

    commit to submit checkpoint

--- oojs.js
+++ oojs.js
@@ -5,10 +5,16 @@
 // - a `songs` property that is an empty array not determined by input (not passed into the constructor)
 // - an `addSong` method that adds a song (string) to the `songs` array
 // Type your solution immediately below this line:
-
-
-
-
+class Playlist {
+  constructor(title) {
+    this.title = title
+    this.songs = []
+  }
+  addSong (songName) {
+    this.songs.push(songName)
+  }
+}
+const shout = new Playlist('Shout')
 
 // #2: Create an instance of the Playlist class and set it to a variable called `myPlaylist`
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
