9d22cba2c60bc1be7403e2d67fecb9004891049a
Mahwash Mahr <mahwash.mahr@gmail.com>
2018-02-12 13:52:49 -0500

    intial commit

--- fundamentals.js
+++ fundamentals.js
@@ -3,29 +3,43 @@
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
 
-
+var foods = ['Fries', 'Pasta', 'Burrittos']
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
 
-
+var last = foods[2]
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
 
-
+var favoriteFoods = []
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
 
+for (i = 0; i < foods.length; i++) {
+    favoriteFoods.push(foods[i])
+}
 
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
 
-
+var instructor {
+    Name: 'Zakk',
+    Gender: 'Male',
+    Cohort: 'WDI21'
+}
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+'has-office-hours'.instructor(if {
+    Yes === true console.log('Available')   
+} 
+        else if {
+            console.log('Not Available')
+        }
+)
