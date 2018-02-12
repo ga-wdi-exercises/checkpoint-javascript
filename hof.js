55600c49b3b01e32ff95a1c36280ca616a4c4130
SAMUEL HAILE <samishken@gmail.com>
2018-02-12 09:54:26 -0500

    hof

--- hof.js
+++ hof.js
@@ -2,15 +2,15 @@
 
 var people = [
   {
-    name: "Layla",
+    name: 'Layla',
     age: 27,
     knownLanguages: 3
   }, {
-    name: "Keanu",
+    name: 'Keanu',
     age: 54,
     knownLanguages: 1
   }, {
-    name: "Jasmine",
+    name: 'Jasmine',
     age: 35,
     knownLanguages: 2
   }
@@ -22,8 +22,8 @@ var people = [
 // Type your solution immediately below this line:
 var peopleNames = people.map(person => (person.name))
 
-
 // #2: Use the `filter` array method to create a new, filtered array containing only
 // persons from the `people` array who know multiple languages. Assign the returned array
 // to a variable called `polyglotPeople`.
 // Type your solution immediately below this line:
+var peopleNames = people.filter(person => (person.name))
