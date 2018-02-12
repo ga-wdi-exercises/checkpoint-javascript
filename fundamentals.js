35f2d7cf7b888fc3c7325c7e9d03d9838afe94d7
Daniel Freudberg <dbfreudber@gmail.com>
2018-02-12 09:30:53 -0500

    answered more questions, think i figured out the error

--- fundamentals.js
+++ fundamentals.js
@@ -18,16 +18,21 @@ var favoriteFoods = []
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
 
-for  (i = 0, i < foods.length, i++) {
+for  (i = 0; i < foods.length; i++) {
     favoriteFoods[i]=foods[i]
 }
-
+console.log(favoriteFoods)
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
 
-
+var instructor = {
+    Name: 'Zakk',
+    Cohort: '21',
+    shirtColor: 'sort of red'
+}
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+
