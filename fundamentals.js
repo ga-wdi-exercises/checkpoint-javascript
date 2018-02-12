03dd42c68179e6442e76916821cf1fd07744502e
Daniel Freudberg <dbfreudber@gmail.com>
2018-02-12 09:08:07 -0500

    3 questions

--- fundamentals.js
+++ fundamentals.js
@@ -13,7 +13,7 @@ var last = "crab"
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
 
-
+var favoriteFoods = []
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
