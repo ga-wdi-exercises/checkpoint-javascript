6e28b6a37f14aea67f563a76a975bab475f30c12
Angela Malone <a.malone1193@gmail.com>
2018-02-12 09:40:09 -0500

    finished hof

--- hof.js
+++ hof.js
@@ -20,6 +20,9 @@ var people = [
 // person in the `people` array. Assign the returned array to a variable
 // called `peopleNames`.
 // Type your solution immediately below this line:
+for (i = 0; i < people.length; i++) { 
+  var peopleNames = people.map(name);
+}
 
 
 
@@ -27,3 +30,6 @@ var people = [
 // persons from the `people` array who know multiple languages. Assign the returned array
 // to a variable called `polyglotPeople`.
 // Type your solution immediately below this line:
+var polyglotPeople = people.filter(function(people) {
+  return people.knownLanguages > 1;
+})
