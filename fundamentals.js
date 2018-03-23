aa9b6326a2a5dc4818b7219669bfeb1366349a94
Meron Kebede <kebedemeron905@gmail.com>
2018-03-23 09:51:58 -0400

    (add javascript checkpoint)

--- fundamentals.js
+++ fundamentals.js
@@ -3,29 +3,37 @@
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
 
-
+var foods = ['appel', 'muffin', 'pizza']
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
 
-
+foods[2]
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
 
-
+var favoriteFoods = []
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
 
-
-
+var foods = ['appel', 'muffin', 'pizza']
+var favoriteFoods = []
+for (var i = 0; i < foods.length; i++) {
+  favoriteFoods.push(foods[i])
+}
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
 
-
+var instructor = {
+  name: 'Sam',
+  cohort: '21',
+  age: '30'
+}
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+instructor['has-office-hours'] = '4 - 5'
--- test/hof_test.js
+++ test/hof_test.js
@@ -1,55 +1,52 @@
-var should = require("chai").should()
-var expect = require("chai").expect
-var fs = require("fs")
-var hof = fs.readFileSync("hof.js","utf8")
+var should = require('chai').should()
+var expect = require('chai').expect
+var fs = require('fs')
+var hof = fs.readFileSync('hof.js', 'utf8')
 
-describe('HOF #1', function() {
+describe('HOF #1', function () {
   let section = hof.slice(
     0,
     hof.indexOf('// #2')
   )
   try {
     eval(section)
-  }
-  catch(e) {
+  } catch (e) {
     console.log(e)
   }
-  it('peopleNames should be an array', function() {
+  it('peopleNames should be an array', function () {
     expect(peopleNames).to.be.an('array')
   })
-  it('.map should be called on `people`', function() {
+  it('.map should be called on `people`', function () {
     expect(hof).to.include('people.map')
   })
-  it('peopleNames should contain the names of each person in the people array', function() {
-    let names = ["Layla", "Keanu", "Jasmine"]
+  it('peopleNames should contain the names of each person in the people array', function () {
+    let names = ['Layla', 'Keanu', 'Jasmine']
     expect(peopleNames).to.deep.equal(names)
   })
 })
 
-describe('HOF #2', function() {
+describe('HOF #2', function () {
   let section =
     hof.slice(
       0,
       hof.indexOf('// #1')
-    )
-    +
+    )    +
     hof.slice(
       hof.indexOf('// #2')
     )
   try {
     eval(section)
-  }
-  catch(e) {
+  } catch (e) {
     console.log(e)
   }
-  it('polyglotPeople is an array', function() {
+  it('polyglotPeople is an array', function () {
     expect(polyglotPeople).to.be.an('array')
   })
-  it('.filter should be called on `people`', function() {
+  it('.filter should be called on `people`', function () {
     expect(hof).to.include('people.filter')
   })
-  it('polyglotPeople only contains persons who know multiple languages', function() {
-    let expectedPeople = people.filter((v,i) => i!==1)
+  it('polyglotPeople only contains persons who know multiple languages', function () {
+    let expectedPeople = people.filter((v, i) => i !== 1)
     expect(polyglotPeople).to.deep.equal(expectedPeople)
   })
 })
