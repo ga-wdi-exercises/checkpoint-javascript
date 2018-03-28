77a35af27a757d3df23029a833ef1e4fdf7a3fae
Minhquan Thai <minhquanthai@gmail.com>
2018-03-28 08:26:31 -0400

    oojs.js

--- oojs.js
+++ oojs.js
@@ -10,17 +10,15 @@ class Playlist {
     this.title = title
     this.songs = songs
   }
-  addSong() {
-
+  addSong (Playlist) {
+    Playlist.push(songs)
   }
 }
 
 // #2: Create an instance of the Playlist class and set it to a variable called `myPlaylist`
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 // Type your solution immediately below this line:
-
-
-
+var myPlaylist = new Playlist('title', [])
 
 
 // NOTE: THE CODE BELOW IS FOR TESTING PURPOSES. DO NOT REMOVE OR ALTER.
