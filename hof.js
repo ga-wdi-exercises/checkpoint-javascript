60c86753cb29aa5d64b5fadff594284b1766104e
Marissa Wood <marissa.espina@gmail.com>
2018-05-25 09:58:53 -0400

    changed variable name for more clarity in HOF test #1

--- hof.js
+++ hof.js
@@ -21,7 +21,7 @@ var people = [
 // called `peopleNames`.
 // Type your solution immediately below this line:
 var peopleNames = []
-people.map(function(name) {peopleNames.push(name.name)})
+people.map(function(person) {peopleNames.push(person.name)})
 
 // #2: Use the `filter` array method to create a new, filtered array containing only
 // persons from the `people` array who know multiple languages. Assign the returned array
