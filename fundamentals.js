24f817ec4e30cb29716b5f460a75ae818a98f73d
Michelle Monfort <Michellemnfrt@gmail.com>
2018-05-28 21:23:40 -0400

    revisit to complete

--- fundamentals.js
+++ fundamentals.js
@@ -7,18 +7,27 @@ var foods = ['apples', 'pears', 'bananas']
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-var lastItem = anArray.pop();
+var last = foods[foods.length - 1]
 
+// document.getElementById.foods.lastIndexOf
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
+var favoriteFoods = []
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
-
+for (let i = 0; i < foods.length; i++) {
+  favoriteFoods.push(foods[i])
+}
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
-
+var instructor = {
+  'firstName': 'Jack',
+  'startAge': 'forty',
+  'intialHeight': 'tall'
+}
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+instructor['has-office-hours'] = true
