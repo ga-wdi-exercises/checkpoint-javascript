9fe2a3543ba887551de7d5c257461cd4052dffd4
Lawrence Kirk <lmkirkiii@gmail.com>
2018-07-09 09:34:26 -0400

    added a people array

--- test/fundamentals_test.js
+++ test/fundamentals_test.js
@@ -12,6 +12,10 @@ var instructor = {
     robot: 'dog',
     'has-office-hours': true
 }
+var peopleNames = ['john','joe','jim'];
+var people = ['john','joe','jim'];
+people.map();
+
 
 
 describe('Fundamentals #1', function() {
