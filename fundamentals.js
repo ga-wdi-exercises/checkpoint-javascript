47d0345f46f0fa8c0ec1c531db5d2a42ca6d7590
“GRACE <GRACE.CHEUNG@YALE.EDU>
2018-09-10 09:37:51 -0400

    oojs

--- fundamentals.js
+++ fundamentals.js
@@ -1,31 +1,40 @@
-// NOTE: Make sure to use the `var` keyword for ALL variable declarations.
+// // NOTE: Make sure to use the `var` keyword for ALL variable declarations.
 
-// #1: Create an array of strings called `foods` that contains three foods.
-// Type your solution immediately below this line:
+// // #1: Create an array of strings called `foods` that contains three foods.
+// // Type your solution immediately below this line:
 
+// var foods = ['cucumber', 'tomato', 'banana']
 
+// // #2: Access the last item in the array and assign to a variable called `last`.
+// // Type your solution immediately below this line:
 
-// #2: Access the last item in the array and assign to a variable called `last`.
-// Type your solution immediately below this line:
+// var last = foods[2]
 
+// // #3: Create an empty array called `favoriteFoods`.
+// // Type your solution immediately below this line:
 
+// var favoriteFoods = []
 
-// #3: Create an empty array called `favoriteFoods`.
-// Type your solution immediately below this line:
+// // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
+// // Type your solution immediately below this line:
 
-
-
-// #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
-// Type your solution immediately below this line:
-
-
-
-// #5: Create an object literal called `instructor` that contains three key-value pairs.
-// Type your solution immediately below this line:
-
+// for (i = 0; i < foods.length; i++ ) {
+//     favoriteFoods.push(foods[i])
+// }
 
 
-// #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
-// it (do not change the original object you typed above) and assigning it
-// a boolean value.
-// Type your solution immediately below this line:
+// // #5: Create an object literal called `instructor` that contains three key-value pairs.
+// // Type your solution immediately below this line:
+
+// var instructor { 
+//     name: Bob,
+//     height: 63,
+//     hairColor: brown
+// }
+
+// // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
+// // it (do not change the original object you typed above) and assigning it
+// // a boolean value.
+// // Type your solution immediately below this line:
+
+// instructor()
--- oojs.js
+++ oojs.js
@@ -6,17 +6,23 @@
 // - an `addSong` method that adds a song (string) to the `songs` array
 // Type your solution immediately below this line:
 
-
-
-
+class Playlist {
+  constructor (title, songs) {
+    this.title = title
+    this.songs = []
+    }
+    addSong (song) {
+      this.songs.push(song)
+      console.log(this.songs)
+  }
+}
 
 // #2: Create an instance of the Playlist class and set it to a variable called `myPlaylist`
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 // Type your solution immediately below this line:
 
-
-
-
+var myPlaylist = new Playlist('this is my playlist title')
+myPlaylist.addSong('song')
 
 // NOTE: THE CODE BELOW IS FOR TESTING PURPOSES. DO NOT REMOVE OR ALTER.
 if(typeof Playlist !== 'undefined') {
