ec94cea7621405ca7416844b415d2debf161f764
Marichka Tsiuriak <marichka.ts94@gmail.com>
2018-09-10 10:03:50 -0400

    oojs

--- oojs.js
+++ oojs.js
@@ -6,7 +6,16 @@
 // - an `addSong` method that adds a song (string) to the `songs` array
 // Type your solution immediately below this line:
 
+class Playlist {
+  constructor(title) {
+    this.title = title
+    this.songs = []
+  }
 
+  addSong() {
+    this.songs.push(this.title)
+  }
+}
 
 
 
@@ -14,7 +23,7 @@
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 // Type your solution immediately below this line:
 
-
+var myPlaylist = new Playlist('This is a song');
 
 
 
