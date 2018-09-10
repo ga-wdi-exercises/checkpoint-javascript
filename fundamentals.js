8ac31cc780ad046e56c0da5a75180aec6b076990
Camille Garcia <camgarcia53@gmail.com>
2018-09-10 09:44:25 -0400

    first part of checkpoint test

--- fundamentals.js
+++ fundamentals.js
@@ -3,29 +3,36 @@
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
 
+var foods = ["Pasta", "Cake", "Pizza"]
 
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-
-
+var last = foods.pop()
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
 
-
+var favoriteFoods = []
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
 
+foods.shift(for (i = 0; i < foods.length; i++))
 
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
 
+var instructor = { personality: "Kind",
+                   attitude: "Flamboyant",
+                   lunch: "Soup"
 
+                            }
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+
+instructor['has-office-hours'] = "True"
