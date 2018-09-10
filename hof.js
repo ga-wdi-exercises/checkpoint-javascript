3d7b955561692c1120a468fbf9291e549c2d3a40
Sathya Ram <sathyatheram@gmail.com>
2018-09-10 09:49:36 -0400

    fundementals completed, hof almost complete

--- hof.js
+++ hof.js
@@ -21,9 +21,14 @@ var people = [
 // called `peopleNames`.
 // Type your solution immediately below this line:
 
-
+people.map(function() {
+  var peopleNames = [];
+  peopleNames = people.name;
+});
 
 // #2: Use the `filter` array method to create a new, filtered array containing only
 // persons from the `people` array who know multiple languages. Assign the returned array
 // to a variable called `polyglotPeople`.
 // Type your solution immediately below this line:
+
+people.filter() 
--- oojs.js
+++ oojs.js
@@ -7,15 +7,23 @@
 // Type your solution immediately below this line:
 
 
-
-
+class Playlist {
+  constructor(title) {
+    this.title = title;
+    this.songs = [];
+  }
+  addSong() {
+    songs.push("Fury");
+  }
+};
 
 // #2: Create an instance of the Playlist class and set it to a variable called `myPlaylist`
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 // Type your solution immediately below this line:
 
-
-
+var myPlaylist = new Playlist("Hello");
+addSong();
+console.log(myPlaylist)
 
 
 // NOTE: THE CODE BELOW IS FOR TESTING PURPOSES. DO NOT REMOVE OR ALTER.
