59a4caa32d583034f13355c039b9d072c107f777
Luis Ortiz <ortizlu@icloud.com>
2018-09-10 09:26:28 -0400

    fourth problem

--- fundamentals.js
+++ fundamentals.js
@@ -13,17 +13,19 @@ var last = foods[2]
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
 
-var favorit
+var favoriteFoods = []
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
 
-
+for (var i = 0; i < foods.length; i++) {
+  favoriteFoods.push(foods[i])
+}
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
 
-
+var
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
