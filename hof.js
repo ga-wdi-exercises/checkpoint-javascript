b4976ffa766c5dd191e31e90d5561a401795ed46
Mike Bell <michaelbell.va@gmail.com>
2018-09-10 09:25:48 -0400

    fundamentals.js

--- hof.js
+++ hof.js
@@ -20,7 +20,9 @@ var people = [
 // person in the `people` array. Assign the returned array to a variable
 // called `peopleNames`.
 // Type your solution immediately below this line:
-
+var peopleNames = people.map((person) => {
+  return person.name
+})
 
 
 // #2: Use the `filter` array method to create a new, filtered array containing only
