45c4b72905d70b4b39aa89868fda7a04d1a002bc
McEwen Hardy <mcewenhardy@gmail.com>
2018-09-17 08:04:30 -0400

    -

--- hof.js
+++ hof.js
@@ -5,34 +5,50 @@ var people = [
     name: "Layla",
     age: 27,
     knownLanguages: 3
-  }, {
+  },
+  {
     name: "Keanu",
     age: 54,
     knownLanguages: 1
-  }, {
+  },
+  {
     name: "Jasmine",
     age: 35,
     knownLanguages: 2
   }
-]
+];
 
 // #1: Use the `map` array method to create a new array containing the names of each
 // person in the `people` array. Assign the returned array to a variable
 // called `peopleNames`.
 // Type your solution immediately below this line:
 
-var peopleNames = (function(people)
-  document.people.map('name')
-})
-return peopleNames
-
+var peopleNames = [];
+for (let i = 0; i < people.length; i++) {
+  let firstName = name[i];
+  let names = people.name;
+  peopleNames.push(names);
+  return peopleNames;
+}
 
 // #2: Use the `filter` array method to create a new, filtered array containing only
 // persons from the `people` array who know multiple languages. Assign the returned array
 // to a variable called `polyglotPeople`.
 // Type your solution immediately below this line:
+function multipleLanguages(knownLanguages) {
+  return people.knownLanguages > 1;
+}
 
-var polyglotPeople = (function(people)
-document.people.filter('knownLanguages' > 1){
-})
-return polyglotPeople
+const moreThan1 = [];
+for (let i = 0; i < people.length; i++) {
+  if (multipleLanguages(people[i])) {
+    moreThan1.push(people[i]);
+  }
+}
+const multiple = people.filter(knownLanguages => {
+  return people.knownLanguages > 1;
+});
+
+// = people.filter(people.knownLanguages);
+// if (knownLanguages > 1) {
+//   console.log(multipleLanguages);
--- oojs.js
+++ oojs.js
@@ -5,26 +5,44 @@
 // - a `songs` property that is an empty array not determined by input (not passed into the constructor)
 // - an `addSong` method that adds a song (string) to the `songs` array
 // Type your solution immediately below this line:
-var Playlist class = {
-  constructor ('title',)
-  this.title = title
-  this.addsong = addsong
+
+class Playlist {
+  constructor(title, songs = []) {
+    this.title = title
+    this.songs = songs
+  }
 }
-songs
+
+function addSong(track) {
+  Playlist.songs.push(track)
+
+  addsong(track)
+  track = 'spiderman'
+}
+console.log(Playlist.songs)
+
+// song = 'string'
 
 
+// var Playlist class = {
+//   constructor('title')
+//   this.title = title
+//   this.addsong = addsong
+// }
+// addsong('this')
+// console.log(songs)
 
 // #2: Create an instance of the Playlist class and set it to a variable called `myPlaylist`
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 // Type your solution immediately below this line:
 
-
+// var myPlayList = Playlist
 
 
 
 // NOTE: THE CODE BELOW IS FOR TESTING PURPOSES. DO NOT REMOVE OR ALTER.
-if(typeof Playlist !== 'undefined') {
+if (typeof Playlist !== "undefined") {
   module.exports = {
     Playlist
-  }
+  };
 }
