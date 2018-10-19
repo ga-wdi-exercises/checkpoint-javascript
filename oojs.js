c4fef30f1948eddc0a71551083508685602c6262
AprilLee11 <mikyung.lee11@gmail.com>
2018-10-19 09:41:59 -0400

    checkpoint

--- oojs.js
+++ oojs.js
@@ -6,21 +6,25 @@
 // - an `addSong` method that adds a song (string) to the `songs` array
 // Type your solution immediately below this line:
 
-
-
-
+class Playlist {
+  constructor(title) {
+    this.title;
+    this.songs = [];
+  }
+  addSong(song) {
+    this.songs.push(song);
+  }
+}
 
 // #2: Create an instance of the Playlist class and set it to a variable called `myPlaylist`
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 // Type your solution immediately below this line:
-
-
-
-
+var myPlaylist = new Playlist("mylist");
+myPlaylist.addSong("No title");
 
 // NOTE: THE CODE BELOW IS FOR TESTING PURPOSES. DO NOT REMOVE OR ALTER.
-if(typeof Playlist !== 'undefined') {
+if (typeof Playlist !== "undefined") {
   module.exports = {
     Playlist
-  }
+  };
 }
