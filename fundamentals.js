37572e4ab95d9e367aac7f102f47dc3982f2c623
Tishana <tishana.trainor@gmail.com>
2018-10-19 09:35:18 -0400

    First commit

--- fundamentals.js
+++ fundamentals.js
@@ -3,27 +3,29 @@
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
 
-
+var foods = ["spaghetti", "pizza", "apple"];
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-
-
+var last = foods[foods.length - 1];
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
-
-
+var favoriteFoods = [];
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
-
-
+for (i = 0; i < foods.length; i++) {
+  favoriteFoods.push(foods[i]);
+}
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
-
-
+var instructor = {
+  name: "Hector",
+  course: "WDI",
+  years: 2
+};
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
--- oojs.js
+++ oojs.js
@@ -6,7 +6,15 @@
 // - an `addSong` method that adds a song (string) to the `songs` array
 // Type your solution immediately below this line:
 
-
+class Playlist {
+  constructor(title, songs){
+    this.title = title;
+    this.songs = [];
+  }
+  addSong: function (song){
+    this.songs.push(song);
+  }
+};
 
 
 
@@ -14,7 +22,7 @@
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 // Type your solution immediately below this line:
 
-
+var myPlaylist = new Playlist('Toxicity')
 
 
 
