0d3a696ac2fd552acab1b236feccbc4845b0682b
Lovdeep <lovdeepgahunia1@gmail.com>
2018-10-26 11:33:03 -0400

    final commit

--- oojs.js
+++ oojs.js
@@ -6,21 +6,26 @@
 // - an `addSong` method that adds a song (string) to the `songs` array
 // Type your solution immediately below this line:
 
-
-
-
+class Playlist {
+  constructor(title) {
+    this.title = title;
+    this.songs = [];
+  }
+  addSong(song) {
+    this.songs.push(song);
+  }
+}
 
 // #2: Create an instance of the Playlist class and set it to a variable called `myPlaylist`
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 // Type your solution immediately below this line:
 
-
-
-
+myPlaylist = new Playlist("don's playlist");
+myPlaylist.addSong("songsong");
 
 // NOTE: THE CODE BELOW IS FOR TESTING PURPOSES. DO NOT REMOVE OR ALTER.
-if(typeof Playlist !== 'undefined') {
+if (typeof Playlist !== "undefined") {
   module.exports = {
     Playlist
-  }
+  };
 }
