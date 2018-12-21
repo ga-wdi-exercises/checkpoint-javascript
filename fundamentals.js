5646c9fbcf14ae0b16aca78b4bc73735639b8f47
“MARIA <MDFORTINO@GMAIL.COM>
2018-12-21 09:14:48 -0500

    fundamentals 1

--- fundamentals.js
+++ fundamentals.js
@@ -1,13 +1,14 @@
-// NOTE: Make sure to use the `var` keyword for ALL variable declarations.
+// NOTE: Make sure to use the `var` keyword for ALL variable declarations. //DO NOT USE CONST OR LET
+//**Make a commit after you complete each section.**
+
 
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
-
-
+var foods = ["rice", "chicken", "pork"]
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-
+console.log(foods[2])
 
 
 // #3: Create an empty array called `favoriteFoods`.
