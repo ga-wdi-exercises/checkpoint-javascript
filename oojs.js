f8a8126d0ea21227339033d938f7444d2b7129d6
Roger Campbell II <rc1336@gmail.com>
2018-12-21 09:59:23 -0500

    forgot to commit as I completed each prompt.

--- oojs.js
+++ oojs.js
@@ -5,6 +5,13 @@
 // - a `songs` property that is an empty array not determined by input (not passed into the constructor)
 // - an `addSong` method that adds a song (string) to the `songs` array
 // Type your solution immediately below this line:
+class Playlist  {
+  constructor(title, songs, addSongs){
+    this.title = title,
+    this.songs = [],
+    this.addSongs = songs.push()
+  }
+}
 
 
 
