caa9f1f259de4020d6ff46d5afdcc177bce3a885
Courtney Tyler <courtneytyler7@gmail.com>
2018-12-21 09:59:48 -0500

    add hof attempt1

--- hof.js
+++ hof.js
@@ -28,3 +28,7 @@ var peopleNames = people.map(function() {
 console.log(peopleNames)
 
 
+// #2: Use the `filter` array method to create a new, filtered array containing only
+// persons from the `people` array who know multiple languages. Assign the returned array
+// to a variable called `polyglotPeople`.
+// Type your solution immediately below this line:
