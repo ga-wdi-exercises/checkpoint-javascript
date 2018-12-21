184d1e1c27f9bae9a2a4a439a987420cf8ce4e87
Phat Doan <phatdoan@yahoo.com>
2018-12-21 09:49:56 -0500

    init commit

--- fundamentals.js
+++ fundamentals.js
@@ -3,29 +3,46 @@
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
 
-
+var foods = ["apples", "oranges", "durian"]
+// console.log(foods)
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
 
-
+var last = foods[2]
+// console.log(last)
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
 
-
+var favoriteFoods = []
+// console.log(favoriteFoods)
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
 
+var favoriteFoods = []
+for (var i = 0; i < foods.length; i++)
+favoriteFoods.push(foods[i])
+
+// console.log(favoriteFoods)
 
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
 
+var instructor = {
+    firstName: "Joe",
+    lastName: "Jones",
+    subject: "Math"
+}
 
+// console.log(instructor)
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+
+instructor.has_office_hours = true
+// console.log(instructor)
--- hof.js
+++ hof.js
@@ -21,9 +21,16 @@ var people = [
 // called `peopleNames`.
 // Type your solution immediately below this line:
 
+var peopleNames = people.map((people) => people.name)
 
+// console.log(peopleNames)
 
 // #2: Use the `filter` array method to create a new, filtered array containing only
 // persons from the `people` array who know multiple languages. Assign the returned array
 // to a variable called `polyglotPeople`.
 // Type your solution immediately below this line:
+
+
+var polyglotPeople = people.filter((people) => people.knownLanguages >= 2)
+
+// console.log(polyglotPeople)
