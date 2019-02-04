196f68592716eaaf78bb3d76b91b2c0edce869f2
Paul Blake <pnblake@gmail.com>
2019-02-04 09:42:09 -0500

    Update with solutions to everything except the last prompt in fundamentals.js.

--- fundamentals.js
+++ fundamentals.js
@@ -3,25 +3,39 @@
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
 
+var foods = ["pizza", "yogurt", "cereal"];
 
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
 
+var last = foods[2];
 
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
+
+var favoriteFoods = [];
 
 
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
 
+for (let i = 0; i < foods.length; i++){
+    favoriteFoods.push(foods[i]);
+}
+
 
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
+
+var instructor = {
+    name: "Zakk",
+    height: "tall",
+    hair: "brown"
+}
 
 
 
@@ -29,3 +43,5 @@
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+
+//This can't be done with dot notation, and I'm struggling to remember the other.
