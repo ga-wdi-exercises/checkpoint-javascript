674ebb25895d6656025392523031c389ef7c18d9
Leonard Busenah <busenah@gmail.com>
2019-02-04 09:57:44 -0500

    adding

--- oojs.js
+++ oojs.js
@@ -5,6 +5,8 @@
 // - a `songs` property that is an empty array not determined by input (not passed into the constructor)
 // - an `addSong` method that adds a song (string) to the `songs` array
 // Type your solution immediately below this line:
+class Playlist = (title, songs)
+constructor("song")
 
 
 
@@ -13,6 +15,9 @@
 // #2: Create an instance of the Playlist class and set it to a variable called `myPlaylist`
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 // Type your solution immediately below this line:
+class MiniPlaylist = ("myPlayList", "addSong")
+constructor(tilte, songs);
+super
 
 
 
