3a5c4f9ebe1df403cc70afd3e04edff64aefc0cf
Ryan White <mrwhiterk@gmail.com>
2019-02-04 09:29:24 -0500

    finished test

--- hof.js
+++ hof.js
@@ -1,25 +1,24 @@
 // NOTE: Make sure to use the `var` keyword for ALL variable declarations
 
-var people = [
-  {
-    name: "Layla",
-    age: 27,
-    knownLanguages: 3
-  }, {
-    name: "Keanu",
-    age: 54,
-    knownLanguages: 1
-  }, {
-    name: "Jasmine",
-    age: 35,
-    knownLanguages: 2
-  }
-]
+var people = [{
+  name: "Layla",
+  age: 27,
+  knownLanguages: 3
+}, {
+  name: "Keanu",
+  age: 54,
+  knownLanguages: 1
+}, {
+  name: "Jasmine",
+  age: 35,
+  knownLanguages: 2
+}]
 
 // #1: Use the `map` array method to create a new array containing the names of each
 // person in the `people` array. Assign the returned array to a variable
 // called `peopleNames`.
 // Type your solution immediately below this line:
+var peopleNames = people.map(person => person.name);
 
 
 
@@ -27,3 +26,4 @@ var people = [
 // persons from the `people` array who know multiple languages. Assign the returned array
 // to a variable called `polyglotPeople`.
 // Type your solution immediately below this line:
+var polyglotPeople = people.filter(person => person.knownLanguages > 1);
