9c98bcd969776123e1433ad3b071852d56478966
Yoshi Maisami <yoshi.maisami@gmail.com>
2019-02-04 09:29:31 -0500

    fundemantls 1st attempt

--- fundamentals.js
+++ fundamentals.js
@@ -1,31 +1,50 @@
 // NOTE: Make sure to use the `var` keyword for ALL variable declarations.
 
-// #1: Create an array of strings called `foods` that contains three foods.
+// #1: Creatse an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
 
+
+var foods = ["apples", "oranges", "pear"];
 
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
 
+foods.push("last");
 
+//??
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
 
+var favoriteFoods = [];
 
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
+
+//???
+
+(i = 0; i < foods.length; i++) {
+    
+console.log(favoriteFoods(foods[i]))
 
 
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
 
+var Instructor = {
 
+    intelligence: "smart",
+    patience: "yes",
+    consistent: "yes"
+
+}
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+
+Instructor.shift(`has-office-hours` + `instructor`) 
