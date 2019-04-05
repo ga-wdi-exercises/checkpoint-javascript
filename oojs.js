fd4231f52845f717f99e1a665c23b662a0856504
Daniel Woldehana <daniel.woldehana@yahoo.comM>
2019-04-05 10:16:36 -0400

    getting there

--- oojs.js
+++ oojs.js
@@ -6,8 +6,18 @@
 // - an `addSong` method that adds a song (string) to the `songs` array
 // Type your solution immediately below this line:
 
+class Playlist {
+  constructor(title){
+    this.title = title
+    this.songs = []
+  }
+  addSongs(ph){
+    ph = this.songs.push(ph)
+  }
+}
 
-
+var albums = new Playlist('my Life')
+console.log(albums.title)
 
 
 // #2: Create an instance of the Playlist class and set it to a variable called `myPlaylist`
