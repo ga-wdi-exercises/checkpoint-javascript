75b5fd0ebe770eb8312341d188193b38005f5a70
Amlak Befekadu <amlakbarkon@gmail.com>
2019-04-05 09:14:57 -0400

    #4 is done

--- fundamentals.js
+++ fundamentals.js
@@ -3,12 +3,12 @@
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
 
-
+var foods = ["pasta", "pizza", "orange", "engera"]
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
 
-
+  var last = foods[foods.length -1]
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
