f8749d16c36351fc58808439ae348524ab910d93
Daniel Woldehana <daniel.woldehana@yahoo.comM>
2019-04-05 09:38:08 -0400

    update

--- fundamentals.js
+++ fundamentals.js
@@ -2,18 +2,18 @@
 
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
-var foods = ["pizza", "burger", "hotDogs", "cheeseCake"]
+var foods = ["pizza", "burger", "hotDogs"]
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-var last = foods[3]
+var last = foods[2]
 // console.log(last)
 
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
 var favoriteFoods = []
-
+// console.log(favoriteFoods)
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
