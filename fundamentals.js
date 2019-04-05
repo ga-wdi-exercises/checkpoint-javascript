bf2789e717af4d7221434da66a9a7753bf1f6d1c
Aidan Andrew Oliver Doyle <aodoyle91@gmail.com>
2019-04-05 09:17:21 -0400

    test1

--- fundamentals.js
+++ fundamentals.js
@@ -8,7 +8,7 @@ var foods = ["turkey", "sandwich", "vindaloo"]
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
 
-
+var last = foods[2]
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
