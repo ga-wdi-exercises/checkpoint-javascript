916db1031f293292f84f0cfe88688fea754518d1
Nathan Rotach <Nathan.Rotach@gmail.com>
2019-04-05 09:55:00 -0400

    oojs

--- hof.js
+++ hof.js
@@ -2,15 +2,15 @@
 
 var people = [
   {
-    name: "Layla",
+    name: 'Layla',
     age: 27,
     knownLanguages: 3
   }, {
-    name: "Keanu",
+    name: 'Keanu',
     age: 54,
     knownLanguages: 1
   }, {
-    name: "Jasmine",
+    name: 'Jasmine',
     age: 35,
     knownLanguages: 2
   }
@@ -19,11 +19,13 @@ var people = [
 // #1: Use the `map` array method to create a new array containing the names of each
 // person in the `people` array. Assign the returned array to a variable
 // called `peopleNames`.
-// Type your solution immediately below this line:
-
+// Type your solution immediately below this function getFields(people, field) {
+var peopleNames = people.map(people, name))
 
 
 // #2: Use the `filter` array method to create a new, filtered array containing only
 // persons from the `people` array who know multiple languages. Assign the returned array
 // to a variable called `polyglotPeople`.
 // Type your solution immediately below this line:
+var polyglotPeople = people.filter(kL => kL.knownLanguages.excludes(1))
+console.log(polyglotPeople)
--- oojs.js
+++ oojs.js
@@ -5,7 +5,16 @@
 // - a `songs` property that is an empty array not determined by input (not passed into the constructor)
 // - an `addSong` method that adds a song (string) to the `songs` array
 // Type your solution immediately below this line:
-
+var songs = ['1.']
+class Playlist {
+  constructor(title) {
+    this.title = title
+    this.songs = songs
+  }
+  addSong() {
+    this.songs.push()
+  }
+}
 
 
 
@@ -13,6 +22,8 @@
 // #2: Create an instance of the Playlist class and set it to a variable called `myPlaylist`
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 // Type your solution immediately below this line:
+var myPlaylist = new Playlist ('myPlaylist')
+myPlaylist.addSong('newSong')
 
 
 
