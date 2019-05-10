959480c86774e3018b66ac6a500e1822de98956a
DCartist <pbannerman13@gmail.com>
2019-05-10 09:56:06 -0400

    still adding more even when test stopped working

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 var last = "last"
 var foods = ["food", "hungry", last]
-
 var favoriteFoods = foods
-
 var instructor = {
     ['has - office - hours']: true,
     name: 'Bob Ross',
@@ -10,12 +8,24 @@ var instructor = {
 }
 
 var instructor = {
-        ['has - office - hours,']: true,
-        name: 'Bob Ross',
-        games: 'Resident evil'
+    ['has-office-hours,']: true,
+    name: 'Bob Ross',
+    games: 'Resident evil'
+}
+
+instructor.hope = "yes"
+var peoplesNames = ['bob', 'dick', 'sally']
+var people = peoplesNames.map(function(name) {
+    return name
+})
+
+var polyglotPeople = peoplesNames.filter(name => name)
+var class Playlist {
+    constructor(title, song) {
+        this.title = title
+        this.song = song
     }
-    //instructor.hope = "yes"
-    // var peoplesNames = ['bob', 'dick', 'sally']
-    // var people = peoplesNames.map(function(name) {
-    //     return name
-    // })
+    addSong() {
+
+    }
+}
