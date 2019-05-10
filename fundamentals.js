540c7b9b181d04f5de57bed0a7ede1b3cb8c71d2
Melika <melika.r@gmail.com>
2019-05-10 09:55:55 -0400

    checkpoint-javascript completed

--- fundamentals.js
+++ fundamentals.js
@@ -2,26 +2,33 @@
 
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
-
+var foods = ["soup", "rice", "sandwich"];
 
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-
+var last = foods[2];
 
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
-
+favoriteFoods =[];
 
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
-
+for (var i=0; i<foods.length; i++){
+    favoriteFoods.push(foods[i]);
+}
 
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
+var instructor= {
+    name: "Sara",
+    class: "Chemistry",
+    Campus: "Indiana"
+}
 
 
 
@@ -29,3 +36,4 @@
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+instructor[`has-office-hours`] = true;
