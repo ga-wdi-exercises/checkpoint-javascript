69f9d18593d462c967dd4b74acd803505e1fad1c
Ellison Rio <rio.ellison@gmail.com>
2019-05-15 14:33:10 -0400

    i added two lines for adding songs to playlist, but now more errors than before

--- oojs.js
+++ oojs.js
@@ -21,7 +21,8 @@ class Playlist {
 // #2: Create an instance of the Playlist class and set it to a variable called `myPlaylist`
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 // Type your solution immediately below this line:
-var myPlaylist = new Playlist()
+var myPlaylist = new Playlist("John Mayer Favorites");
+myPlaylist.addSong("Born and Raised");
 
 
 
