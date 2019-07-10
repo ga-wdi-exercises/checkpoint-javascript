73e455004866ad5927467c0fd6e0459917c5d117
ashtinlatting <rlatting92@gwu.edu>
2019-07-10 10:00:06 -0400

    initial commit

--- fundamentals.js
+++ fundamentals.js
@@ -2,27 +2,35 @@
 
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
-
+var foods = ["oatmeal", "eggs", "bacon"]
 
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-
+var last = foods[2]
 
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
+var favoriteFoods = []
 
 
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
 
-
+for (i = 0; i < foods.length; i++) {
+	favoriteFoods.push(foods[i])
+}
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
-
+var instructor (name, age, location) = {
+	name: this.name
+	age: this.age
+	location: this.location
+}
+instrictor("Ashtin", 26, "Vacation")
 
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
--- hof.js
+++ hof.js
@@ -20,6 +20,10 @@ var people = [
 // person in the `people` array. Assign the returned array to a variable
 // called `peopleNames`.
 // Type your solution immediately below this line:
+var peopleNames = []
+people.map(peopleNames)
+peopleNames.push(people[i].name)
+
 
 
 
