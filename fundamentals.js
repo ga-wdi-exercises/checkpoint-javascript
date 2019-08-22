baf859fa9b9a1e14059eada584c501b17d00f82c
Leandre Munyana <lmunyana007@gmail.com>
2019-08-22 10:01:51 -0400

    checkpoint

--- fundamentals.js
+++ fundamentals.js
@@ -2,30 +2,36 @@
 
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
-
+const foods = ["pizza", 'burgers', 'wings'];
 
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-
+const last = foods[2];
 
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
-
-
+const favoriteFoods = []; 
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
-
+for (let i = 0; i <= foods.length; i++);
+favoriteFoods += foods;
 
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
-
-
+var instructor = {
+    Name: "hammad",
+    Age: '30',
+    Title: 'Instructor'
+}
+console.log(instructor);
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+
+instructor.has-office-hours: true;
--- oojs.js
+++ oojs.js
@@ -5,10 +5,20 @@
 // - a `songs` property that is an empty array not determined by input (not passed into the constructor)
 // - an `addSong` method that adds a song (string) to the `songs` array
 // Type your solution immediately below this line:
+class Playlist {
+  constructor(title, addSong) {
+  this.title = title;
+  this.addSong = addSong;
+}
+play() {
+  var song = [];
+  for (let i = 0; i < song.length; i++)
+  song += this.addSong;
+}
+var song = [];
 
 
-
-
+//not coompleted. this was my stopping point
 
 // #2: Create an instance of the Playlist class and set it to a variable called `myPlaylist`
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
