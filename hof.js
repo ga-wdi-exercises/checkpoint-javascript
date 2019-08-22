959914edf7f551f7811c7f97541ff37d75fbe386
Tymothy J Hanks <tymjhanks@gmail.com>
2019-08-22 10:00:56 -0400

    These are my results

--- hof.js
+++ hof.js
@@ -21,9 +21,50 @@ var people = [
 // called `peopleNames`.
 // Type your solution immediately below this line:
 
+for(let i = 0;i<people.length; i++){
+ 
+
+  var newArrayOne = people.map(movingNames)
+  newArrayOne.push(peopleNames);
+  var peopleNames = []
+
+    function movingNames (){
+      
+  }
+  console.log(peopleNames)
+}
+
+
 
 
 // #2: Use the `filter` array method to create a new, filtered array containing only
 // persons from the `people` array who know multiple languages. Assign the returned array
 // to a variable called `polyglotPeople`.
 // Type your solution immediately below this line:
+
+
+// people.knownLanguages > 1
+
+
+
+var newA = [];
+var many = people.filter(knownL)
+
+ function knownL () {
+  if (people.knownLanguages > 1){
+    
+    console.log(people.push(newA))
+
+  }
+ }
+ 
+  // return people.knownLanguages > 1;
+
+
+//  var newA = [];
+ 
+// var many = people.filter(Langk)
+
+// var pushing = many.push(newA)
+
+// Langk();?
