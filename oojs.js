a0d47ac56a110033052c4e97ecae74fbbb9db524
ASHLEY_MILES_TAYLOR <ASHLEY_MILES_TAYLOR@GMAIL.COM>
2019-10-16 09:59:21 -0400

    assessment

--- oojs.js
+++ oojs.js
@@ -7,12 +7,23 @@
 // Type your solution immediately below this line:
 
 
-
+var Playlist = class 
+constructor(title, songs, addSong())
+title = "title",
+songs = [],
+{
+addSong() {
+	songs.push()
+}
+}
 
 
 // #2: Create an instance of the Playlist class and set it to a variable called `myPlaylist`
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 // Type your solution immediately below this line:
+
+myPlaylist
+
 
 
 
