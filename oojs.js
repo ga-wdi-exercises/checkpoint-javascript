920293e815735a8f007f79bb9246d10f928f1ff5
Tre <amack4693@gmail.com>
2019-10-16 10:00:01 -0400

    added answers

--- oojs.js
+++ oojs.js
@@ -6,7 +6,12 @@
 // - an `addSong` method that adds a song (string) to the `songs` array
 // Type your solution immediately below this line:
 
-
+class Playlist{
+  constructor(title, songs)
+  title = title;
+  songs = [];
+ 
+}
 
 
 
