aa3f3f40332f01522a5d4f7cc74a4b18b1685387
Jenna Dean <jenna.dean9@gmail.com>
2019-10-16 09:56:53 -0400

    i miss google

--- hof.js
+++ hof.js
@@ -21,9 +21,16 @@ var people = [
 // called `peopleNames`.
 // Type your solution immediately below this line:
 
+peopleNames.map(people.name);
 
+var peopleNames = [];
 
 // #2: Use the `filter` array method to create a new, filtered array containing only
 // persons from the `people` array who know multiple languages. Assign the returned array
 // to a variable called `polyglotPeople`.
 // Type your solution immediately below this line:
+
+polyglotPeople = [];
+if (people.knownLanguages > 1){
+  polyglotPeople.push();
+}
--- oojs.js
+++ oojs.js
@@ -7,16 +7,24 @@
 // Type your solution immediately below this line:
 
 
-
+class Playlist{
+  constructor(title){
+    this.title = title;
+    this.songs = [];
+  }
+  addSong(){
+    this.songs.push();
+  }
+}
 
 
 // #2: Create an instance of the Playlist class and set it to a variable called `myPlaylist`
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 // Type your solution immediately below this line:
 
+var myPlaylist = new Playlist();
 
-
-
+myPlaylist.addSong("Can't you See");
 
 // NOTE: THE CODE BELOW IS FOR TESTING PURPOSES. DO NOT REMOVE OR ALTER.
 if(typeof Playlist !== 'undefined') {
