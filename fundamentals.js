a56fcbf7395b1f54e6e60b04b1d5df2a7c5c8b3e
William Chrapcynski <wchrapcynski@gmail.com>
2019-10-16 09:32:50 -0400

    fundamentals1

--- fundamentals.js
+++ fundamentals.js
@@ -3,26 +3,22 @@
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
 
-
+var foods = ["eggs", "cheese", "bread"];
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-
 
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
 
 
-
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
 
 
-
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
-
 
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
