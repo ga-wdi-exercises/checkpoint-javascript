80e1a79cd9302ca735f26d53c79e1747f013ccd7
William Chrapcynski <wchrapcynski@gmail.com>
2019-10-16 09:41:43 -0400

    oojs1

--- oojs.js
+++ oojs.js
@@ -7,7 +7,16 @@
 // Type your solution immediately below this line:
 
 
+class Playlist {
+    constructor(title) {
+        this.title = title;
+        this.songs = [];
+    }
 
+    addSong(string) {
+        this.songs.push(string)
+    }
+}
 
 
 // #2: Create an instance of the Playlist class and set it to a variable called `myPlaylist`
