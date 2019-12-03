e326d21d257238480a0f9e36c207668a91be158e
Kelly M Collins <90kcol@gmail.com>
2019-12-03 09:32:11 -0500

    Working Prompt 6 Prompt 5 Done

--- fundamentals.js
+++ fundamentals.js
@@ -23,11 +23,16 @@ var favoriteFoods = [];
 // Type your solution immediately below this line:
 for (var i = 0; i < foods.length; i++) {
   favoriteFoods[i] = foods[i];
-}
+};
 
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
+var instructor = {
+  name : '',
+  place : '',
+  height : ''
+};
 
 
 
@@ -35,3 +40,5 @@ for (var i = 0; i < foods.length; i++) {
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+
+instructor.has-office-hours = 'yes';
