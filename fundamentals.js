3002bcee92bbd3d409afbf9376ef9ecf3433b597
Monquize Dusseau <dusseau.monquize@gmial.com>
2020-02-05 09:39:18 -0500

    Q2

--- fundamentals.js
+++ fundamentals.js
@@ -2,7 +2,7 @@
 
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
-var foods = ['Salmon','Mango','Kale'];
+var foods = ["Salmon","Mango","Kale"];
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
@@ -10,13 +10,13 @@ var last = foods[2];
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
-var favoriteFoods = [];
+// var favoriteFoods = [];
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
-for( let i = 0; i <= foods.length; i++){
-    favoriteFoods.push(foods[i]);
-}
+// for( let i = 0; i <= foods.length; i++){
+//     favoriteFoods.push(foods[i]);
+// }
 
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
