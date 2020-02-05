29658f0a9a0c0c85f09666f866091fde84ab7356
Levani Papashvili <papashvililevani@gmail.com>
2020-02-05 10:34:40 -0500

    Levani

--- fundamentals.js
+++ fundamentals.js
@@ -3,24 +3,36 @@
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
 
-
+ var foods = ["pizza", "lamb", "papaya"]
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
 
+var foods = ["papaya"]
+
+	 
 
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
 
-
+var  favoriteFoods = []
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
 
-
+for (var i = 0; i < foods i++) {
+	foods[i]
+}
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
+
+var instructor ={
+	name: "pairs";
+    has-office-hours: 65;
+    location: "hdgdmkd";
+};
+
 
 
 
