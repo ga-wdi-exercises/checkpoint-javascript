7340ae271565ef185188990704b386106b239c54
Lakshmi Bhavani Inampudi <inampudilakshmibhavani@johndeere.com>
2020-02-12 10:14:12 -0600

    completed

--- oojs.js
+++ oojs.js
@@ -5,7 +5,16 @@
 // - a `songs` property that is an empty array not determined by input (not passed into the constructor)
 // - an `addSong` method that adds a song (string) to the `songs` array
 // Type your solution immediately below this line:
+class Playlist {
+   constructor (title){
+     this.title = title;
+     this.songs = [];
 
+   }
+    addSong (params) {
+    this.songs.push(params);
+   }
+}
 
 
 
@@ -14,8 +23,8 @@
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 // Type your solution immediately below this line:
 
-
-
+var myPlaylist = new Playlist('myPlaylist');
+myPlaylist.addSong('song1');
 
 
 // NOTE: THE CODE BELOW IS FOR TESTING PURPOSES. DO NOT REMOVE OR ALTER.
