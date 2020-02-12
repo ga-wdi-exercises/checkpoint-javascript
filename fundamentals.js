1a9669821247b2862f21f9068d7ba93c4bfb283d
Marc Wright <marc@generalassemb.ly>
2020-02-12 09:23:41 -0600

    4 passing

--- fundamentals.js
+++ fundamentals.js
@@ -9,7 +9,7 @@ var foods = ['pizza', 'sushi', 'ice cream']
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-
+var last = foods[foods.length - 1]
 
 
 // #3: Create an empty array called `favoriteFoods`.
