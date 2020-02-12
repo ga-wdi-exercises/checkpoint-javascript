0f4a96233d66bdf2b26c557c60367362dca45d37
Karrie Dvorak <dvorak.karrie@gmail.com>
2020-02-12 10:37:48 -0600

    completed checkpoint exercise

--- oojs.js
+++ oojs.js
@@ -1,10 +1,18 @@
 // NOTE: Make sure to use the `var` keyword for ALL variable declarations
-
 // #1: Define a `Playlist` class with the following properties:
 // - a `title` property that is a string determined by some input (passed into the constructor)
 // - a `songs` property that is an empty array not determined by input (not passed into the constructor)
 // - an `addSong` method that adds a song (string) to the `songs` array
 // Type your solution immediately below this line:
+
+var song = "MercyMe"
+class Playlist {
+  constructor(title) {
+  this.title = title
+  this.songs = new Array()
+  this.addSong = function(song) { this.songs.push(song)}
+  }
+}
 
 
 
@@ -14,8 +22,8 @@
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 // Type your solution immediately below this line:
 
-
-
+var myPlaylist = new Playlist('title')
+myPlaylist.addSong("Come Alive")
 
 
 // NOTE: THE CODE BELOW IS FOR TESTING PURPOSES. DO NOT REMOVE OR ALTER.
