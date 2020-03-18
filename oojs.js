81940b0a11d58dfef9b2fcd614a13c68dd449a51
Shaqueasha Thompkins <shaqueasha_thompkins@yahoo.com>
2020-03-18 10:40:55 -0400

    Squeak's Java Exercise

--- oojs.js
+++ oojs.js
@@ -6,7 +6,15 @@
 // - an `addSong` method that adds a song (string) to the `songs` array
 // Type your solution immediately below this line:
 
-
+  class playList {
+    constructor(title) {
+      this.titleName = title
+    songs: [" "]
+  }
+  addSong() {
+    return " " + songs;
+    }
+  }
 
 
 
@@ -14,7 +22,7 @@
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 // Type your solution immediately below this line:
 
-
+  myPlaylist = new playList
 
 
 
