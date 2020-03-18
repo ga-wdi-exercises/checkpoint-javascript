f0e15e826828c34351aa0a3682008805788ce19e
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-03-18 10:15:01 -0400

    submitting checkpoint

--- oojs.js
+++ oojs.js
@@ -6,7 +6,15 @@
 // - an `addSong` method that adds a song (string) to the `songs` array
 // Type your solution immediately below this line:
 
-
+class Playlist {
+  constructor(title, songs){
+    this.title = title,
+    this.songs = []
+  }
+  addSong(){
+    this.songs.push("Con Altura")
+  }
+}
 
 
 
@@ -15,7 +23,8 @@
 // Type your solution immediately below this line:
 
 
-
+var myPlaylist = new Playlist ("Latin Pop", "Chantaje");
+myPlaylist.addSong("So Yo")
 
 
 // NOTE: THE CODE BELOW IS FOR TESTING PURPOSES. DO NOT REMOVE OR ALTER.
