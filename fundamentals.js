0f1f3f58cc22dd2450a674666693978e3b7b2e2e
Stone Koffi <stoneskates20@gmail.com>
2020-03-18 10:47:38 -0400

    Stone Koffi Java checkpoint

--- fundamentals.js
+++ fundamentals.js
@@ -2,21 +2,29 @@
 
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
+var foods = ["salmon", "cauliflower","BrownRice"];
 
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-
+if (BrownRice[BrownRice.length-1] ==  'Last'){
+//do something
+}else{
+//something else
+}
 
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
+var favoriteFoods = [0];
 
 
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
-
+for (favoriteFoods i=0; i<100; i++) {
+    System.out.println("i:" + i);
+  }
 
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
