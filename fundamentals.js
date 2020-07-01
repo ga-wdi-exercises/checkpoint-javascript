2c2fda7dca84b9e981d2f85366988a0c7aa0cfe1
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-07-01 11:53:45 -0400

    Add f3

--- fundamentals.js
+++ fundamentals.js
@@ -10,6 +10,7 @@ var last = foods[foods.length - 1];
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
+var favoriteFoods = [];
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
