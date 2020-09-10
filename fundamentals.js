9824fae92201e0f192ab70bd884396dce459a904
Brady Dawley <dawleybradym@johndeere.com>
2020-09-10 08:17:17 -0500

    Finshed Fundamentals: #2

--- fundamentals.js
+++ fundamentals.js
@@ -2,12 +2,12 @@
 
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
-var food = ['pizza', 'cheeseburger', 'tacos'];
+var foods = ['pizza', 'cheeseburger', 'tacos'];
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
 
-
+var last = foods[2];
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
