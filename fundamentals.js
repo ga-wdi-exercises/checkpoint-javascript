1e35a3fa1da61548f8db11e5da3affc1ffbb954b
Nick LaPointe <LaPointeNicholas@JohnDeere.com>
2020-09-10 09:19:18 -0400

    fundamentals 2 complete

--- fundamentals.js
+++ fundamentals.js
@@ -6,7 +6,7 @@ var foods = ['steak','pizza','pasta'];
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-
+var last = foods[2];
 
 
 // #3: Create an empty array called `favoriteFoods`.
