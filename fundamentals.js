7282cc2e4b5ed7d90769da05a8f1a8cd0a226994
Tim Kaiser <kaisertimothy@johndeere.com>
2020-09-10 08:18:57 -0500

    completed fundamentals 2

--- fundamentals.js
+++ fundamentals.js
@@ -2,11 +2,11 @@
 
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
-var foods = [pizza, burger, fries];
+var foods = ["pizza", "burger", "fries"];
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-
+last = foods[foods.length-1];
 
 
 // #3: Create an empty array called `favoriteFoods`.
