03a2f05bd5caad5ad8afff605fb9f76eda031816
ambonetto <bonettoandream@johndeere.com>
2020-09-10 08:19:46 -0500

    Completed Fundamentals Prompt 1

--- fundamentals.js
+++ fundamentals.js
@@ -3,11 +3,11 @@
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
 
+    var foods = ["Ice Cream", "Chocolate", "Pie"];
+    console.log(foods);
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-
-
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
