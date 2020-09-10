f31d6eb9a906af0a9047f415d8b22e6d8c510d22
MD Manning <manningmichaeld@johndeere.com>
2020-09-10 08:17:56 -0500

    prompt 1 f

--- fundamentals.js
+++ fundamentals.js
@@ -2,7 +2,7 @@
 
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
-
+var foods = ["eggs" , "bacon", "coffee"];
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
