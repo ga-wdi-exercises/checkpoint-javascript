ce5eb9c288c83d7dad431e1d9438346da07cf067
ambonetto <bonettoandream@johndeere.com>
2020-09-10 08:32:43 -0500

    Completed Fundamentals Prompt 5

--- fundamentals.js
+++ fundamentals.js
@@ -30,7 +30,13 @@
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
 
+    var instructor = {
+        class: 'English', 
+        yearsExperience: 2,
+        previousJob: 'Yes'
+    };
 
+    console.log(instructor);
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
