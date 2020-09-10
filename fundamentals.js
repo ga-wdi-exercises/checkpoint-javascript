c713f31fc0029b34fbc8d3c8c7d9376e199a0b20
cyoder1 <yodercamdend@johndeere.com>
2020-09-10 08:22:24 -0500

    fund q.3

--- fundamentals.js
+++ fundamentals.js
@@ -12,7 +12,7 @@ foods[2] = last;
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
-
+var favoriteFoods = [];
 
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
