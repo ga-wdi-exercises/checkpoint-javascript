b2463a63504367a877834e4248c4250a4d9db468
David Williams <williamsdavida@johndeere.com>
2020-09-10 08:20:43 -0500

    #5

--- fundamentals.js
+++ fundamentals.js
@@ -23,7 +23,11 @@ for (let i = 0; i < foods.length; i++) {
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
-
+var instructor = {
+    firstName: "David",
+    lastName: "Williams",
+    subject: "Math"
+}
 
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
