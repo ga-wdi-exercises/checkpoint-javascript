f13ca1559d20bff62eb928e320df73c65df9c220
Chris Myers <myerschristopher@johndeere.com>
2020-09-10 08:27:26 -0500

    Fundamentals 6

--- fundamentals.js
+++ fundamentals.js
@@ -2,7 +2,7 @@
 
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
-var food = [peaches, cream, asparagus];
+var food = ['peaches', 'cream', 'asparagus'];
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
@@ -34,3 +34,4 @@ var instructor = {
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+instructor['has-office-hours'] = true;
