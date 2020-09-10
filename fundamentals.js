124351a035bdd7f39ab75ff2f7dfd0db6782ac66
dd86185 <DietrichDarciJ@JohnDeere.com>
2020-09-10 08:19:56 -0500

    Casey is made

--- fundamentals.js
+++ fundamentals.js
@@ -24,7 +24,11 @@ for(let i = 0; i < foods.length; i++) {
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
 
-
+var instructor = {
+    fname: "Casey",
+    lname: "Harding",
+    nname: "Thor"
+};
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
