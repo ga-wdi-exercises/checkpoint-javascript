b3804928474ae3d98dd5bcdc2a0ed4362df0fbd9
jonnyv91 <vaflorjonathan@johndeere.com>
2020-09-10 08:36:09 -0500

    #6

--- fundamentals.js
+++ fundamentals.js
@@ -17,7 +17,7 @@ favoriteFoods = [];
 // Type your solution immediately below this line:
 for ( i = 0; i < foods.length; i++) {
     favoriteFoods.push(foods[i]);
-}
+};
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
@@ -25,10 +25,11 @@ var instructor = {
     name: "Jon",
     gender: "male",
     age: 28,
-}
+};
 
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+instructor["has-office-hours"] = true;
