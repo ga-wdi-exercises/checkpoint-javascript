ff8e5cb816bbc00b4caa01497c24da814bff2133
Nick LaPointe <LaPointeNicholas@JohnDeere.com>
2020-09-10 09:31:23 -0400

    all prompts completed

--- fundamentals.js
+++ fundamentals.js
@@ -17,7 +17,7 @@ var favoriteFoods = [];
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
 for (let i =0; i < foods.length; i++) {
-    favoriteFoods.push(foods[i]);
+    favoriteFoods.push(foods[i]); 
 };
 
 
