5b8a85f6e4deb82785cd6d5f03a097d99d7684fe
Chris Myers <myerschristopher@johndeere.com>
2020-09-10 08:20:13 -0500

    Fundamentals 2

--- fundamentals.js
+++ fundamentals.js
@@ -6,7 +6,7 @@ var food = [peaches, cream, asparagus];
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-
+var last = food[2];
 
 
 // #3: Create an empty array called `favoriteFoods`.
