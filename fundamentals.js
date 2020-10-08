8e53c33740d22a8be7bc7d7598fbf279830fdca0
lina mierkE <lina_maria89@hotmail.com>
2020-10-08 16:23:05 -0400

    5

--- fundamentals.js
+++ fundamentals.js
@@ -23,7 +23,11 @@ for ( i=0; i <= foods.length; i++){
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
-
+var instructor = {
+    firstName: "Bob",
+    lastName: "Smith",
+    age: 100    
+};
 
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
