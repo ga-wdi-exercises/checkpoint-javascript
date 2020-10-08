8faea495ba25283a242efb1dba514580410537de
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2020-10-08 13:08:34 -0700

    prompt 2 & 3

--- fundamentals.js
+++ fundamentals.js
@@ -6,12 +6,12 @@ var foods = ["apple", "banana", "blackberry"]
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-
+var last = foods[2]
 
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
-
+var favoriteFoods = []
 
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
