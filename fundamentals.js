ee50c7422fa66f3bec7666035cc6db9d94f71d17
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-10-08 16:28:52 -0400

    fund 5

--- fundamentals.js
+++ fundamentals.js
@@ -23,7 +23,11 @@ for (let i=0; i < foods.length; i++) {
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
-
+var instructor = {
+    Name: 'Lily',
+    Month: 'October',
+    Day: 'Thursday'
+}
 
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
