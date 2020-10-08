4fdfdbbe4985816ea28471296becb43e12ca6c7d
Mark OBrien <obrienmark@johndeere.com>
2020-10-08 08:20:08 -0500

    prompt 5

--- fundamentals.js
+++ fundamentals.js
@@ -21,8 +21,11 @@ for(let i=0;i<foods.length;i++)
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
-
-
+var instructor = {
+    name:"Peter",
+    knowledge:"High",
+    level:"Expert"
+};
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
