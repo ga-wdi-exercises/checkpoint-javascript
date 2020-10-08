1570482bd134f8b084b72d869a890321ab1ca1e1
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2020-10-08 13:25:11 -0700

    prompt 5

--- fundamentals.js
+++ fundamentals.js
@@ -22,7 +22,11 @@ for (let i=0; i<foods.length; i++);
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
-
+var instructor = {
+    nice: true,
+    number: 3,
+    location: "east coast"
+}
 
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
