2a45ccf27127c914b85e4c0e5b8a16dceec0f2b4
Guadalupe Stephens <stephens.guadalupe@gmail.com>
2020-10-08 16:44:13 -0400

    prompt 5 completed

--- fundamentals.js
+++ fundamentals.js
@@ -18,11 +18,13 @@ favoriteFoods = [];
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
-
+var i = foods;
+for (i= (favoriteFoods), 
 
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
+var instructor = {name: guap, age:27, loves:food};
 
 
 
