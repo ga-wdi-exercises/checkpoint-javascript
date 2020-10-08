ed90db506274c6eebafd7892364a942253db100d
Mark OBrien <obrienmark@johndeere.com>
2020-10-08 08:35:34 -0500

    fixed errors with hyphen and case sensitivity, call cases pass now

--- oojs.js
+++ oojs.js
@@ -5,7 +5,7 @@
 // - a `songs` property that is an empty array not determined by input (not passed into the constructor)
 // - an `addSong` method that adds a song (string) to the `songs` array
 // Type your solution immediately below this line:
-class PlayList{
+class Playlist{
   constructor(title){
     this.title=title;
     this.songs=[];
@@ -18,8 +18,8 @@ class PlayList{
 // #2: Create an instance of the Playlist class and set it to a variable called `myPlaylist`
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 // Type your solution immediately below this line:
-
-
+var myPlaylist = new Playlist("mark");
+myPlaylist.addSong("push");
 
 
 
