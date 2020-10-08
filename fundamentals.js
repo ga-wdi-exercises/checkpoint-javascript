9f99acca2cff4255d665b8ebeb07802dcf419af3
Matthew Lewis <matthew.knight.lewis@gmail.com>
2020-10-08 16:05:38 -0400

    prompt 1 done

--- fundamentals.js
+++ fundamentals.js
@@ -2,11 +2,12 @@
 
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
+var foods = ['pizza', 'pasta', 'garlic bread'];
 
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-
+//var last = foods[foods.length-1];
 
 
 // #3: Create an empty array called `favoriteFoods`.
